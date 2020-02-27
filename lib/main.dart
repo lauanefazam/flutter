a0d89a235f93e1e93ac85a1c1ec0e0783a8da9cf
lauanefazam <lauane.francisco@etec.sp.gov.br>
2020-02-27 12:37:52 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(
+      MaterialApp(
+          home: Center(
+        child: Container(
+          color: Colors.pink[500],
+          child: Text("Lauane Fazam", style: TextStyle(color: Colors.white)),
+        ),
+      )),
+    );
+
